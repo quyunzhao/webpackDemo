ab267b45444d671cce1874d6cd203bc351b79123
jingqu <1264352733@qq.com>
2020-11-14 23:17:06 +0800

    React 创建组件

--- src/index.js
+++ src/index.js
@@ -2,5 +2,28 @@
 import React from "react"; //创建组件、虚拟DOM元素、生命周期
 import ReactDOM from "react-dom"; // 把创建好的 组件 和 虚拟DOM  放到页面上
 
+// 第一种创建组件的方式
+// 构造函数 首字母大写
+function Hello(props) {
+  // return null表示此组件为空，什么都不会渲染
+  // return null;
+  console.log(props);
+  return <div>这个是Hello组件~~~~{props.name}</div>;
+}
+const user = {
+  name: "小明",
+  age: "22",
+  area: "china",
+  id: "1001",
+};
+// 第二种 创建组件的方式
+
 //使用 ReactDOM  将虚拟DOM 渲染到页面上
-ReactDOM.render(<div>123</div>, document.getElementById("app"));
+ReactDOM.render(
+  <div>
+    123
+    {/* 直接把 组件 的 名称 ，以标签的形式丢到页面上 */}
+    <Hello name={user.name} age={user.age} area={user.area}></Hello>
+  </div>,
+  document.getElementById("app")
+);
