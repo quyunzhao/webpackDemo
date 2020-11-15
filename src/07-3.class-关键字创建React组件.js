b485e4c52feb7a53b97350f29fcd58d9a11e429d
jingqu <1264352733@qq.com>
2020-11-15 20:23:54 +0800

    class 关键字创建 React 组件

--- src/07-3.class-关键字创建React组件.js
+++ src/07-3.class-关键字创建React组件.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
 
 // class 关键字创建组件
-class Movie extends Component {
+// class Movie extends Component {
+//   // render函数的作用 是渲染当前组件所对的 虚拟DOM元素
+//   render() {
+//     return <div> 这里是Movie组件的内容</div>;
+//     // return null;
+//   }
+// }
+
+class Movie extends React.Component {
   // render函数的作用 是渲染当前组件所对的 虚拟DOM元素
   render() {
     return <div> 这里是Movie组件的内容</div>;
