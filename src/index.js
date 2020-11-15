c842bfd85b5391153d2b2cf524072c8fa32faaa7
jingqu <1264352733@qq.com>
2020-11-15 20:22:42 +0800

    class 关键字创建 React 组件

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import React from "react"; //创建组件、虚拟DOM元素、生命周期
 import ReactDOM from "react-dom"; // 把创建好的 组件 和 虚拟DOM  放到页面上
 
 // 导入 class
-import "@/07.class-继承";
+import Movie from "@/07-3.class-关键字创建React组件";
 
 //使用 ReactDOM  将虚拟DOM 渲染到页面上
-ReactDOM.render(<div>123</div>, document.getElementById("app"));
+ReactDOM.render(
+  <div>
+    123
+    <hr />
+    <Movie></Movie>
+  </div>,
+  document.getElementById("app")
+);
