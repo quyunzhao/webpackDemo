88ccc34003c60b2b373063d67c4422b97b30e7c6
jingqu <1264352733@qq.com>
2020-11-18 20:09:59 +0800

    添加行内样式

--- src/components/CmtItem.jsx
+++ src/components/CmtItem.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 
+const itemStyle = {
+  border: "1px dashed #ccc",
+  margin: "10px",
+  padding: "10px",
+  boxShadow: "0 0 10px #ccc",
+};
+
+const userStyle = {
+  fontSize: "14px",
+};
+
+const contentStyle = {
+  fontSize: "12px",
+};
 // 使用 function 定义普通的无状态组件
 // 子组件
 function CmtItem(props) {
   return (
-    <div
-      style={{
-        border: "1px dashed #ccc",
-        margin: "10px",
-        padding: "10px",
-        boxShadow: "0 0 10px #ccc",
-      }}
-    >
-      <h1 style={{ fontSize: "14px" }}>评论人：{props.user}</h1>
-      <h2 style={{ fontSize: "12px" }}>评论内容：{props.content}</h2>
+    <div style={itemStyle}>
+      <h1 style={userStyle}>评论人：{props.user}</h1>
+      <h2 style={contentStyle}>评论内容：{props.content}</h2>
       {/* <hr /> */}
     </div>
   );
