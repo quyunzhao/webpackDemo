5fbbeea4cb74f7a868682ff51d88910c6b58b723
jingqu <1264352733@qq.com>
2020-11-18 21:28:13 +0800

    为普通样式表启用模块化

--- src/components/CmtItem2.jsx
+++ src/components/CmtItem2.jsx
@@ -6,9 +6,11 @@ console.log(cssobj);
 // 子组件
 function CmtItem(props) {
   return (
-    <div>
-      <h1 id={cssobj.cmtTitle}>评论人：{props.user}</h1>
-      <h2>评论内容：{props.content}</h2>
+    <div className={cssobj.cmtBox}>
+      {/* <h1 id={cssobj.cmtTitle}>评论人：{props.user}</h1> */}
+      <h1 className={cssobj.title}>评论人：{props.user}</h1>
+
+      <h2 className={cssobj.content}>评论内容：{props.content}</h2>
     </div>
   );
 }
