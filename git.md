d3e4ccf4db9c68c5052e1334dab32ccf0f38746c
jingqu <1264352733@qq.com>
2020-11-21 10:15:40 +0800

    绑定事件 2.0

--- git.md
+++ git.md
@@ -4,6 +4,6 @@ git pull
 
 git add .
 
-git commit -m "绑定事件"
+git commit -m "绑定事件 2.0"
 
 git push
--- src/components/BindEvent.jsx
+++ src/components/BindEvent.jsx
@@ -14,7 +14,17 @@ export default class BindEvent extends React.Component {
         <hr />
         {/* 驼峰命名 */}
         {/* <button onClick={function(){console.log("ok")}}>按钮</button> */}
-        <button onClick={this.myClickHandler}>按钮</button>
+        {/* 函数抽离1.0 */}
+        {/* <button onClick={this.myClickHandler}>按钮</button> */}
+        {/* 注意： onClick 只接受 function 作为处理函数 */}
+        {/* 箭头函数本身就是一个匿名的 function 函数 */}
+        <button
+          onClick={() => {
+            this.myClickHandler();
+          }}
+        >
+          按钮
+        </button>
       </div>
     );
   }
