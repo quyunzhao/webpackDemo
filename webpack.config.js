7dc5e6d9fb43f58c6112626d3a93b8046218033f
jingqu <1264352733@qq.com>
2023-05-27 19:03:29 +0800

    模块热替换

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,23 @@
 const path = require("path");
-const ForkTsCheckerNotifierWebpackPlugin = require("fork-ts-checker-notifier-webpack-plugin");
-const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = (env) => {
-  return {
-    mode: "development",
-    entry: "./src/index.ts",
-    output: {
-      filename: "bundle.js",
-      path: path.resolve(__dirname, "dist"),
-      clean: true,
-    },
-    resolve: {
-      extensions: [".tsx", ".ts", ".js"],
-      extensionAlias: {
-        ".ts": [".js", ".ts"],
-        ".cts": [".cjs", ".cts"],
-        ".mts": [".mjs", ".mts"],
-      },
-    },
-    module: {
-      rules: [
-        {
-          test: /.([cm]?ts|tsx)$/,
-          loader: "ts-loader",
-        },
-      ],
-    },
-    plugins: [
-      new ForkTsCheckerWebpackPlugin(),
-      new ForkTsCheckerNotifierWebpackPlugin({
-        title: "TypeScript",
-        excludeWarnings: false,
-      }),
-      new HtmlWebpackPlugin(),
-    ],
-  };
+module.exports = {
+  entry: {
+    app: "./src/index.js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Hot Module Replacement",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
 };
