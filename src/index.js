74124d1b072d426ed62b11aaa7a9e31dd4888f8b
jingqu <1264352733@qq.com>
2023-05-27 13:14:37 +0800

    回退处理

--- src/index.js
+++ src/index.js
@@ -1,38 +1,10 @@
 import _ from "lodash";
-import "./style.css";
-import Icon from "./icon.png";
-
-import Data from "./data.xml";
-import Notes from "./data.csv";
-
-import toml from "./data.toml";
-import yaml from "./data.yaml";
-import json from "./data.json5";
 
 function component() {
   const element = document.createElement("div");
 
   // lodash 在当前 script 中使用 import 引入
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
-
-  // 将图像添加到我们已经存在的 div 中。
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  console.log(Data);
-  console.log(Notes);
-
-  console.log(toml.title); // output `TOML Example`
-  console.log(toml.owner.name); // output `Tom Preston-Werner`
-
-  console.log(yaml.title); // output `YAML Example`
-  console.log(yaml.owner.name); // output `Tom Preston-Werner`
-
-  console.log(json.title); // output `JSON5 Example`
-  console.log(json.owner.name); // output `Tom Preston-Werner`
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,10 @@
 const path = require("path");
 
-const toml = require("toml");
-const yaml = require("yamljs");
-const json5 = require("json5");
-
 module.exports = {
   entry: "./src/index.js",
   mode: "production",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-  },
-  module: {
-    rules: [
-      // css 样式文件
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      // 图片
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      // 字体
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-      },
-      // csv | tsv 文件
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ["csv-loader"],
-      },
-      // xml 文件
-      {
-        test: /\.xml$/i,
-        use: ["xml-loader"],
-      },
-      // 自定义 JSON 模块 parser start
-      {
-        test: /\.toml$/i,
-        type: "json",
-        parser: {
-          parse: toml.parse,
-        },
-      },
-      {
-        test: /\.yaml$/i,
-        type: "json",
-        parser: {
-          parse: yaml.parse,
-        },
-      },
-      {
-        test: /\.json5$/i,
-        type: "json",
-        parser: {
-          parse: json5.parse,
-        },
-      },
-      // 自定义 JSON 模块 parser end
-    ],
   },
 };
