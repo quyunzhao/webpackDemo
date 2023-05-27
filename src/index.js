d4d7c6706957dcc555115fa00cc2f19d940b16c7
jingqu <1264352733@qq.com>
2023-05-27 17:36:46 +0800

    模块标识符(module identifier)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-async function getComponent() {
+import _ from "lodash";
+
+import Print from "./print";
+
+function component() {
   const element = document.createElement("div");
-  const { default: _ } = await import("lodash");
+
+  // lodash 是由当前 script 脚本 import 进来的
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.onclick = Print.bind(null, "Hello webpack!");
+
   return element;
 }
 
-getComponent().then((component) => {
-  document.body.appendChild(component);
-});
+document.body.appendChild(component());
