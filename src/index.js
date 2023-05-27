b991e0015994c0d632000df6641b186245f7470a
jingqu <1264352733@qq.com>
2023-05-27 17:45:02 +0800

    创建 library

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import _ from "lodash";
+import numRef from "./ref.json";
 
-// import Print from "./print";
-
-function component() {
-  const element = document.createElement("div");
-
-  // lodash 是由当前 script 脚本 import 进来的
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-  // element.onclick = Print.bind(null, "Hello webpack!");
-
-  return element;
+export function numToWord(num) {
+  return _.reduce(
+    numRef,
+    (accum, ref) => {
+      return ref.num === num ? ref.word : accum;
+    },
+    ""
+  );
 }
 
-document.body.appendChild(component());
+export function wordToNum(word) {
+  return _.reduce(
+    numRef,
+    (accum, ref) => {
+      return ref.word === word && word.toLowerCase() ? ref.num : accum;
+    },
+    -1
+  );
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,14 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: {
     index: "./src/index.js",
   },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Caching",
-    }),
-  ],
   output: {
-    filename: "[name].[contenthash].js",
+    filename: "webpack-numbers.js",
     path: path.resolve(__dirname, "dist"),
+    library: "webpackNumbers",
     clean: true,
-  },
-  optimization: {
-    moduleIds: "deterministic",
-    runtimeChunk: "single",
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: "vendors",
-          chunks: "all",
-        },
-      },
-    },
   },
 };
