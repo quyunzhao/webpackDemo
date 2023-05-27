41b92e56171589e81051449e5402d4950f7968b6
jingqu <1264352733@qq.com>
2023-05-27 13:01:19 +0800

    加载数据

--- webpack.config.js
+++ webpack.config.js
@@ -9,17 +9,30 @@ module.exports = {
   },
   module: {
     rules: [
+      // css 样式文件
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      // 图片
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
+      // 字体
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
+      },
+      // csv | tsv 文件
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ["csv-loader"],
+      },
+      // xml 文件
+      {
+        test: /\.xml$/i,
+        use: ["xml-loader"],
       },
     ],
   },
