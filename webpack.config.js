6f195bd6ec5f29c0920f8181be41e2ea85ef41b1
jingqu <1264352733@qq.com>
2023-05-27 18:24:50 +0800

    构建性能TypeScript loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+const ForkTsCheckerNotifierWebpackPlugin = require("fork-ts-checker-notifier-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = (env) => {
   // Use env.<YOUR VARIABLE> here:
@@ -7,11 +10,35 @@ module.exports = (env) => {
 
   return {
     mode: "development",
-    entry: "./src/index.js",
+    entry: "./src/index.ts",
     output: {
       filename: "bundle.js",
       path: path.resolve(__dirname, "dist"),
       clean: true,
     },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+      extensionAlias: {
+        ".ts": [".js", ".ts"],
+        ".cts": [".cjs", ".cts"],
+        ".mts": [".mjs", ".mts"],
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /.([cm]?ts|tsx)$/,
+          loader: "ts-loader",
+        },
+      ],
+    },
+    plugins: [
+      new ForkTsCheckerWebpackPlugin(),
+      new ForkTsCheckerNotifierWebpackPlugin({
+        title: "TypeScript",
+        excludeWarnings: false,
+      }),
+      new HtmlWebpackPlugin(),
+    ],
   };
 };
