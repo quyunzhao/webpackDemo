7077321e0de381a28e8d06003bd5ce607cd31b74
jingqu <1264352733@qq.com>
2023-05-27 20:17:57 +0800

    Shimming 预置依赖

--- webpack.common.js
+++ webpack.common.js
@@ -1,18 +1,22 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
   entry: {
     app: "./src/index.js",
   },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: "Production",
     }),
+    new webpack.ProvidePlugin({
+      join: ["lodash", "join"],
+    }),
   ],
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
 };
