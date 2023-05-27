3be4d4475d113c0f396d42b1bb76d772d65c1c14
jingqu <1264352733@qq.com>
2023-05-27 13:51:46 +0800

    webpack-dev

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,14 @@ module.exports = {
     index: "./src/index.js",
     print: "./src/print.js",
   },
+
+  // 使用 source map
   devtool: "inline-source-map",
+
+  // 修改配置文件，告知 dev server，从什么位置查找文件：
+  devServer: {
+    static: "./dist",
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: "Development",
@@ -17,5 +24,8 @@ module.exports = {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+  },
+  optimization: {
+    runtimeChunk: "single",
   },
 };
