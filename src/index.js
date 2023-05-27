de5378a46605e70a80d9e1f964ece11f1d4f42d1
jingqu <1264352733@qq.com>
2023-05-27 14:09:03 +0800

    动态导入(dynamic import)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-function getComponent() {
-  return import("lodash")
-    .then(({ default: _ }) => {
-      const element = document.createElement("div");
-
-      element.innerHTML = _.join(["Hello", "webpack"], " ");
-
-      return element;
-    })
-    .catch((error) => "An error occurred while loading the component");
+async function getComponent() {
+  const element = document.createElement("div");
+  const { default: _ } = await import("lodash");
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  return element;
 }
-
-getComponent().then((component) => {
-  document.body.appendChild(component);
-});
