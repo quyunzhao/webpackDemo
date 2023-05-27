bc9ba0b78f3f7efb9ba3ab0059acb00bd59a013c
jingqu <1264352733@qq.com>
2023-05-27 20:37:50 +0800

    TypeScript

--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,12 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const webpack = require("webpack");
 
 module.exports = {
   entry: {
-    polyfills: "./src/polyfills",
-    index: "./src/index.js",
+    index: "./src/index.ts",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
   },
   output: {
     filename: "[name].bundle.js",
@@ -15,22 +16,15 @@ module.exports = {
   module: {
     rules: [
       {
-        test: require.resolve("./src/index.js"),
-        use: "imports-loader?wrapper=window",
-      },
-
-      {
-        test: require.resolve("./src/globals.js"),
-        use: "exports-loader?type=commonjs&exports=file,multiple|helpers.parse|parse",
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: "Production",
-    }),
-    new webpack.ProvidePlugin({
-      join: ["lodash", "join"],
     }),
   ],
 };
