7acf881efe30f34f8767cc3216fb877161c0bac0
jingqu <1264352733@qq.com>
2023-05-28 10:47:00 +0800

    out put

--- webpack.common.js
+++ webpack.common.js
@@ -1,10 +1,9 @@
-const path = require("path");
-// const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 // ServiceWorkers
-// const WorkboxPlugin = require("workbox-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -14,11 +13,6 @@ module.exports = {
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
-  },
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
   },
   module: {
     rules: [
@@ -45,7 +39,7 @@ module.exports = {
     ],
   },
   plugins: [
-    // new HtmlWebpackPlugin({ template: "./src/index.html" }),
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
     // new WorkboxPlugin.GenerateSW({
     //   // 这些选项帮助快速启用 ServiceWorkers
     //   // 不允许遗留任何“旧的” ServiceWorkers
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,14 @@
+const path = require("path");
 const { merge } = require("webpack-merge");
+
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "production",
   // devtool: "source-map",
+  output: {
+    filename: "[name].[contenthash].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
 });
