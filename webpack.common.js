76277cd0dfa043b6ecbec22125d3d156ea8b9ec2
jingqu <1264352733@qq.com>
2023-05-28 10:34:28 +0800

    entry 高级用法

--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,16 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 // ServiceWorkers
 // const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
   entry: {
-    index: "./src/index.ts",
+    // index: "./src/index.ts",
+    home: ["./src/home.js", "./src/home.scss"],
+    account: ["./src/account.js", "./src/account.scss"],
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
@@ -18,6 +23,17 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.scss$/,
+        use: [
+          // fallback to style-loader in development
+          process.env.NODE_ENV !== "production"
+            ? "style-loader"
+            : MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
         test: /\.tsx?$/,
         use: "ts-loader",
         exclude: /node_modules/,
@@ -29,12 +45,15 @@ module.exports = {
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+    // new HtmlWebpackPlugin({ template: "./src/index.html" }),
     // new WorkboxPlugin.GenerateSW({
     //   // 这些选项帮助快速启用 ServiceWorkers
     //   // 不允许遗留任何“旧的” ServiceWorkers
     //   clientsClaim: true,
     //   skipWaiting: true,
     // }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
   ],
 };
